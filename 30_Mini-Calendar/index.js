94739794e0a3d4ae2b7d4607ecf9c64b461c97dc
MrSumitKumar <72176567+MrSumitKumar@users.noreply.github.com>
2024-09-04 16:47:30 +0530

    update

--- 30_Mini-Calendar/index.js
+++ 30_Mini-Calendar/index.js
@@ -11,8 +11,8 @@ date_tag.innerText = date.getDate();
 year_tag.innerText = date.getFullYear();
 
 
-const now_month = date.toLocaleString("en", { month: "long",});
-const now_weekday = date.toLocaleString("en", { weekday: "long",});
+const now_month = date.toLocaleString("en", { month: "long"});
+const now_weekday = date.toLocaleString("en", { weekday: "long"});
 
 month_tag.innerText = now_month;
 day_tag.innerText = now_weekday;
