2f8d9b3fc5c7fd41006f113c734821df26df4fa3
MrSumitKumar <72176567+MrSumitKumar@users.noreply.github.com>
2024-09-13 18:39:51 +0530

    add project 38

---
+++ 38_Basic_Counter/script.js
@@ -0,0 +1,10 @@
+
+function minus_btn() {
+    let a = document.getElementById('result').innerText;
+    document.getElementById('result').innerText = parseInt(a) - 1;
+}
+
+function add_btn() {
+    let a = document.getElementById('result').innerText;
+    document.getElementById('result').innerText = parseInt(a) + 1;
+}
